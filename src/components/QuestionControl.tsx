66e5bcba2322292e522f53776ba54ca5ab6bea28
jakechaley <jakechaley@gmail.com>
2023-01-12 15:57:33 -0800

    log answer to console when button is pressed

--- src/components/QuestionControl.tsx
+++ src/components/QuestionControl.tsx
@@ -1,19 +1,23 @@
 import React from "react";
 
+
 class QuestionControl extends React.Component {
   constructor(props: string) {
     super(props);
     this.state = {}
   }
-
+  
+  
   handleClick = () => {
-    console.log("Click")
+    const answersArray = ['yes', 'no', 'maybe', 'absolutely not', 'looking positive']
+    const random = Math.floor(Math.random() * answersArray.length);
+    console.log(random, answersArray[random]);
   }
 
   render() {
     return (
       <React.Fragment>
-        <input type="text" placeholder="Ask Me a Question"/>
+        <input type="text" placeholder="Ask Me a Question" required/>
         <button onClick={this.handleClick} >Give Me an Answer</button>
       </React.Fragment>
     );
